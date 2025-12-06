1234694433d29ec412b9af500f55048183965f5b
Ayas Ibrahim <aayasabraham@gmail.com>
2025-12-06 11:16:23 +0600

    dynamic title some change

--- app/(public)/ad/page.tsx
+++ app/(public)/ad/page.tsx
@@ -1,10 +1,10 @@
 import Advertisement from "@/components/Advertisment";
-// import DynamicTitleFavicon from "@/components/DynamicTitleFavicon";
+import DynamicTitleFavicon from "@/components/DynamicTitleFavicon";
 
 export default function Adpage() {
   return (
     <>
-     
+      <DynamicTitleFavicon title="Advertisement" faviconUrl="/favicon.ico" />
       <Advertisement />
     </>
   );
